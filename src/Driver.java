ae55e6f00949c9cff17583daf5586d0c7e688d8b
Paul <galolopaul7184@gmail.com>
2025-09-19 12:23:00 -0600

    Added test drivers and debug mode for compareTo method which is still in progress

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,7 @@
 public class Driver {
     public static void main(String[] args) {
 
+        Product p = new Product();
         Product[] P = new Product[6];
 
         P[0] = new Product("F001", "Beef Steak", 12.99, Category.Meat);
@@ -8,12 +9,12 @@ public class Driver {
         P[2] = new Product("F003", "Apple", 0.99, Category.Fruits);
         P[3] = new Product("F004", "Carrot", 0.79, Category.Vegetables);
         P[4] = new Product("F005", "Chicken Breast", 8.50, Category.Meat);
-        P[5] = new Product("F006", "Milk", 2.89, Category.Dairy);
+        P[5] = new Product("F006", "Milk", 0.23, Category.Dairy);
 
         // Output results
-        //for (Product x : P) {
-        //    System.out.println(x);
-        //}
+        for (Product x : P) {
+            System.out.println(x);
+        }
 
         // Setup password
         P[0].setPassword("XYZ");
@@ -31,6 +32,9 @@ public class Driver {
         P[0].setPrice(19.98);
 
         // Updated Record
-        //System.out.println(P[0].toString());
+        System.out.println(P[0].toString());
+
+        // Debug for compareTo Method
+        p.compareTo(P);
     }
 }
