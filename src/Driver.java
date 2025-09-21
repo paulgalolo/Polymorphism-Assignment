b90e93628c7b6b8e93e9e39ecadbfc989b7d4b80
Paul <galolopaul7184@gmail.com>
2025-09-20 20:06:26 -0600

    Added code to output the cheapest items

--- src/Driver.java
+++ src/Driver.java
@@ -6,35 +6,40 @@ public class Driver {
 
         P[0] = new Product("F001", "Beef Steak", 12.99, Category.Meat);
         P[1] = new Product("F002", "Cheddar Cheese", 5.49, Category.Dairy);
-        P[2] = new Product("F003", "Apple", 0.99, Category.Fruits);
+        P[2] = new Product("F003", "Apple", 0.01, Category.Fruits);
         P[3] = new Product("F004", "Carrot", 0.79, Category.Vegetables);
-        P[4] = new Product("F005", "Chicken Breast", 8.50, Category.Meat);
-        P[5] = new Product("F006", "Milk", 0.23, Category.Dairy);
+        P[4] = new Product("F005", "Chicken Breast", 0.12, Category.Meat);
+        P[5] = new Product("F006", "Milk", 0.0001, Category.Dairy);
 
         // Output results
         for (Product x : P) {
             System.out.println(x);
         }
 
-        // Setup password
-        P[0].setPassword("XYZ");
+        //// Setup password
+        //P[0].setPassword("XYZ");
+        //
+        //// Lock the product
+        //P[0].Lock("XYZ");
+        //
+        //// First Product Record
+        //System.out.println(P[0].toString());
+        //
+        //// Unlocking the Product
+        //P[0].Unlock("XYZ");
+        //
+        //// Unlock the first product using wrong password
+        //P[0].setPrice(19.98);
+        //
+        //// Updated Record
+        //System.out.println(P[0].toString());
 
-        // Lock the product
-        P[0].Lock("XYZ");
 
-        // First Product Record
-        System.out.println(P[0].toString());
+        Product productInstance = new Product(); // Required to call your instance method
+        Product[] lowest = productInstance.compareTo(P);
 
-        // Unlocking the Product
-        P[0].Unlock("XYZ");
-
-        // Unlock the first product using wrong password
-        P[0].setPrice(19.98);
-
-        // Updated Record
-        System.out.println(P[0].toString());
-
-        // Debug for compareTo Method
-        p.compareTo(P);
+        System.out.println("\nTwo lowest-priced products:");
+        System.out.println(lowest[0]);
+        System.out.println(lowest[1]);
     }
 }
